1a6c8decf101eb012a771cd0a614714a84689957
Gunjan K <kumargunjan14062004@gmail.com>
2026-05-01 05:58:32 +0530

    debug: check mongo uri in logs

--- app/db.py
+++ app/db.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MONGO_URI = os.getenv("MONGO_URI")
+print("MONGO_URI:", os.getenv("MONGO_URI"))
 
 client = MongoClient(MONGO_URI)
 db = client["task_manager"]
